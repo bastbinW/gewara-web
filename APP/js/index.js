1beb6264710e6a3d3c30b14e522a04ad4ea0a275
bastbinW <bastbin@gmail.com>
2017-05-17 16:54:05 +0800

    添加了index.html头部

---
+++ APP/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bin on 2017/5/16.
+ */
+var menuclick = true;
+$("#header-menu").click(function () {
+    if(menuclick){
+        $("#header-menu-pulldown").css("visibility", "visible");
+        menuclick = false;
+    }else{
+        $("#header-menu-pulldown").css("visibility", "hidden");
+        menuclick = true;
+    };
+});
